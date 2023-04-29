480f627e0fda60832644ad79aa1167f4e16733c4
mhdi_nzari <mahdi.nazari.gb008@gmail.com>
2023-04-29 15:34:58 +0330

    update problem after design

--- src/js/App.js
+++ src/js/App.js
@@ -53,51 +53,54 @@ const swiperProjects = new Swiper(".swiper", {
 
 
 const swiperDeals = new Swiper(".swiper-deals", {
-     // Optional parameters
-     direction: "horizontal",
-     loop: true,
-     // Default parameters
-     slidesPerView: 3,
-     spaceBetween: 50,
-     autoplay: {
-       delay: 2500,
-     },
-     // If we need pagination
-     pagination: {
-       el: ".swiper-pagination",
-     },
-   
-     // Navigation arrows
-     navigation: {
-       nextEl: ".swiper-button-next",
-       prevEl: ".swiper-button-prev",
-     },
-   
-     breakpoints: {
-       // when window width is >= 0px
-       0: {
-         slidesPerView: 1,
-         spaceBetween: 70,
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 50,
+  autoplay: {
+    delay: 2500,
+  },
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
 
-       },
-       // when window width is >= 480px
-       480: {
-         slidesPerView: 1,
-         spaceBetween: 70,
-
-       },
-       // when window width is >= 640px
-       764: {
-         slidesPerView: 2,
-         spaceBetween: 70,
-       },
-       1024: {
-         slidesPerView: 3,
-         spaceBetween: 70,
-       },
-   
-     
-     },
-   });
+  breakpoints: {
+    // when window width is >= 0px
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 70,
+      navigation: {
+        prevEl: ".swiper-button-prev",
+      },
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 70,
+      navigation: {
+        prevEl: ".swiper-button-prev",
+      },
+    },
+    // when window width is >= 640px
+    764: {
+      slidesPerView: 2,
+      spaceBetween: 70,
+      navigation: {
+        prevEl: ".swiper-button-prev",
+      },
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 70,
+      // Navigation arrows
+      navigation: {
+        prevEl: ".swiper-button-prev",
+      },
+    },
+  },
+});
 
 
